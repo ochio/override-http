4f9119be46b957ae65e554d14e167a58b0baa545
ochi <ocos54ce@gmail.com>
2023-12-06 23:53:45 +0900

    set up server

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import subprocess
 import browsers
 from browsers.osx import OSX_BROWSER_BUNDLE_LIST
 
+from http.server import HTTPServer,BaseHTTPRequestHandler
+
+class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write('hello'.encode())
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        print(self.rfile.read(int(self.headers['content-length'])).decode('utf-8'))
+        html_context = "完了"
+        self.wfile.write(html_context.encode())
+
 def open_with_default_browser(default_browser_bundle_id,url):
     for browser, bundle_id, version_string in OSX_BROWSER_BUNDLE_LIST:
         if default_browser_bundle_id.lower() == bundle_id.lower():
@@ -10,7 +27,10 @@ def open_with_default_browser(default_browser_bundle_id,url):
 def main():
     default_browser_bundle_id = subprocess.getoutput(cmd="sh getDefaultBrowser.sh")
 
-    # TODO: 別タブ遷移を検知
+    # TODO: ブラウザから遷移先をPOSTされる。それを元にデフォルトのブラウザで開く。
+    server_address = ('localhost', 8080)
+    httpd = HTTPServer(server_address, CustomHTTPRequestHandler)
+    httpd.serve_forever()
 
     open_with_default_browser(default_browser_bundle_id=default_browser_bundle_id, url="https://github.com/ochio")
     
